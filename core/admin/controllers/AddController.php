7c3a91953cb04d76f3b4b1523641876c68309c10
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-01-31 22:17:49 +0100

    Lesson 41

--- core/admin/controllers/AddController.php
+++ core/admin/controllers/AddController.php
@@ -10,8 +10,49 @@ class AddController extends BaseAdmin
         if (!$this->userId) $this->execBase();
         $this->createTableData();
         $this->createForeignData();
+        $this->createRadio();
         $this->createOutputData();
         //$this->model->showForeignKeys($this->table);
+    }
+    protected function createForeignProperty($arr, $rootItems) {
+        if (in_array($this->table, $rootItems['tables'])) {
+            $this->foreignData[$arr['COLUMN_NAME']][0]['id'] = 0;
+            $this->foreignData[$arr['COLUMN_NAME']][0]['name'] = $rootItems['name'];
+        }
+        $columns = $this->model->showColumns($arr['REFERENCED_TABLE_NAME']);
+        $name = '';
+        if ($columns['name']) {
+            $name = 'name';
+        } else {
+            foreach($columns as $key => $value) {
+                if (strpos($key, 'name') !== 'name') {
+                    $name = "$key as name";
+                }
+            }
+            if (!$name) {
+                $name = "{$columns['id_row']} as name";
+            }
+        }
+        if ($this->data) {
+            if ($arr['REFERENCED_TABLE_NAME'] === $this->table) {
+                $where[$this->columns['id_row']] = $this->data[$this->columns['id_row']];
+                $operand[] = '<>';
+            }
+        }
+        $foreign = $this->model->get($arr['REFERENCED_TABLE_NAME'], [
+            'fields' => [$arr['REFERENCED_COLUMN_NAME'] . ' as id', $name],
+            'where' => $where,
+            'operand' => $operand
+        ]);
+        if ($foreign) {
+            if ($this->foreignData[$arr['COLUMN_NAME']]) {
+                foreach ($foreign as $value) {
+                    $this->foreignData[$arr['COLUMN_NAME']][] = $value;
+                }
+            } else {
+                $this->foreignData[$arr['COLUMN_NAME']] = $foreign;
+            }
+        }
     }
     protected function createForeignData($settings = false) {
         if (!$settings) {
@@ -21,81 +62,13 @@ class AddController extends BaseAdmin
         $keys = $this->model->showForeignKeys($this->table);
         if ($keys) {
             foreach ($keys as $item) {
-                if (in_array($this->table, $rootItems['tables'])) {
-                    $this->foreignData[$item['COLUMN_NAME']][0]['id'] = 0;
-                    $this->foreignData[$item['COLUMN_NAME']][0]['name'] = $rootItems['name'];
-                }
-                $columns = $this->model->showColumns($item['REFERENCED_TABLE_NAME']);
-                $name = '';
-                if ($columns['name']) {
-                    $name = 'name';
-                } else {
-                    foreach($columns as $key => $value) {
-                        if (strpos($key, 'name') !== 'name') {
-                            $name = "$key as name";
-                        }
-                    }
-                    if (!$name) {
-                        $name = "{$columns['id_row']} as name";
-                    }
-                }
-                if ($this->data) {
-                    if ($item['REFERENCED_TABLE_NAME'] === $this->table) {
-                        $where[$this->columns['id_row']] = $this->data[$this->columns['id_row']];
-                        $operand[] = '<>';
-                    }
-                }
-                $foreign[$item['COLUMN_NAME']] = $this->model->get($item['REFERENCED_TABLE_NAME'], [
-                    'fields' => [$item['REFERENCED_COLUMN_NAME'] . ' as id', $name],
-                    'where' => $where,
-                    'operand' => $operand
-                ]);
-                if ($foreign[$item['COLUMN_NAME']]) {
-                    if ($this->foreignData[$item['COLUMN_NAME']]) {
-                        foreach ($foreign[$item['COLUMN_NAME']] as $value) {
-                            $this->foreignData[$item['COLUMN_NAME']][] = $value;
-                        }
-                    } else {
-                        $this->foreignData[$item['COLUMN_NAME']] = $foreign[$item['COLUMN_NAME']];
-                    }
-                }
+                $this->createForeignProperty($item, $rootItems);
             }
         } elseif($this->columns['parent_id']) {
-            if (in_array($this->table, $rootItems['tables'])) {
-                $this->foreignData['parent_id'][0]['id'] = 0;
-                $this->foreignData['parent_id'][0]['name'] = $rootItems['name'];
-            }
-            $name = '';
-            if ($this->columns['name']) {
-                $name = 'name';
-            } else {
-                foreach($this->columns as $key => $value) {
-                    if (strpos($key, 'name') !== false) {
-                        $name = "$key as name";
-                    }
-                }
-                if (!$name) {
-                    $name = "{$this->columns['id_row']} as name";
-                }
-            }
-            if ($this->data) {
-                $where[$this->columns['id_row']] = $this->data[$this->columns['id_row']];
-                $operand[] = '<>';
-            }
-            $foreign = $this->model->get($this->table, [
-                'fields' => [$this->columns['id_row'] . ' as id', $name],
-                'where' => $where,
-                'operand' => $operand
-            ]);
-            if ($foreign) {
-                if ($this->foreignData['parent_id']) {
-                    foreach ($foreign as $value) {
-                        $this->foreignData['parent_id'][] = $value;
-                    }
-                } else {
-                    $this->foreignData['parent_id'] = $foreign;
-                }
-            }
+            $arr['COLUMN_NAME'] = 'parent_id';
+            $arr['REFERENCED_COLUMN_NAME'] = $this->columns['id_row'];
+            $arr['REFERENCED_TABLE_NAME'] = $this->table;
+            $this->createForeignProperty($arr, $rootItems);
         }
         return;
     }
--- core/admin/controllers/BaseAdmin.php
+++ core/admin/controllers/BaseAdmin.php
@@ -146,4 +146,17 @@ abstract class BaseAdmin extends BaseController
 
         return false;
     }
+    protected function createRadio($settings=false) {
+        if (!$settings) {
+            $settings = Settings::instance();
+        }
+        $radio = $settings::get('radio');
+        if($radio) {
+            foreach ($this->columns as $name => $item) {
+                if ($radio[$name]) {
+                    $this->foreignData[$name] = $radio[$name];
+                }
+            }
+        }
+    }
 }
