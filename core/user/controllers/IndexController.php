502f16bf9da77598fd4b6c9d6ed8dea7f391e41b
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-02-08 14:17:40 +0100

    Lesson 79-test

--- core/user/controllers/IndexController.php
+++ core/user/controllers/IndexController.php
@@ -1,6 +1,7 @@
 <?php
 namespace core\user\controllers;
 use core\base\controllers\BaseController;
+use core\user\model\Model;
 
 class IndexController extends BaseUser
 {
@@ -8,6 +9,24 @@ class IndexController extends BaseUser
      * @throws \core\base\exceptions\RouteException
      */
     protected function inputData(){
+        $model = Model::instance();
+        $res = $model->get('goods', [
+            'where' => ['id' => '1,2'],
+            'operand' => ['IN'],
+            'join' => [
+                'goods_filter' => ['on' => ['id', 'goods_id']],
+                'filters f' => [
+                    'fields' => ['name as filter_name', 'content'],
+                    'on' => ['filter_id', 'id']
+                ],
+                [
+                    'table' => 'filters',
+                    'on' => ['parent_id', 'id']
+                ]
+            ],
+            'join_structure' => true,
+            'order' => 'RAND()'
+        ]);
         parent::inputData();
     }
 
