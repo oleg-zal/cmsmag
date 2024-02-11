c2f1f1fdbbb5aff1ab46cba1611158310557fa53
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-02-11 15:58:49 +0100

    Lesson 92

--- core/admin/views/js/scripts.js
+++ core/admin/views/js/scripts.js
@@ -14,3 +14,50 @@ function createSiteMap() {
             createSiteMap()
         })
 }
+let files = document.querySelectorAll('input[type=file]');
+let fileStore = [];
+if (files.length ) {
+    files.forEach(item => {
+        item.onchange = function () {
+            let multiple = false;
+            let parentContainer;
+            let container;
+            if (item.hasAttribute('multiple')) {
+                multiple = true;
+                parentContainer = this.closest('.gallery_container');
+                if (!parentContainer) return false;
+                container = parentContainer.querySelectorAll('.empty_container');
+                if (container.length < this.files.length) {
+                    for (let index=0; index < this.files.length - container.length; index++) {
+                        let el = document.createElement('div');
+                        el.classList.add('vg-dotted-square', 'vg-center', 'empty_container');
+                        parentContainer.append(el);
+                    }
+                    container = parentContainer.querySelectorAll('.empty_container');
+                }
+            }
+            let fileName = item.name;
+            let attributeName = item.name.replace(/[\[\]]]/g, '');
+            for (let i in this.files) {
+                if (this.files.hasOwnProperty(i)) {
+                    if (multiple) {
+
+                    }
+                    else {
+                        container = this.closest('.img_container').querySelector('.img_show');
+                        showImage(this.files[i], container);
+                    }
+                }
+            }
+        }
+    })
+    function showImage(item, container) {
+        let reader = new FileReader();
+        container.innerHTML = '';
+        reader.readAsDataURL(item);
+        reader.onload = e => {
+            container.innerHTML = '<img class="img_item" src="">';
+            container.querySelector('img').setAttribute('src', e.target.result);
+        }
+    }
+}
