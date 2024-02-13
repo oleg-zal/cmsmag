4e99b2ca44d489a20cb10e58d61f1c2e8f1e5d9f
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-02-13 20:51:11 +0100

    Lesson 98-last

--- core/admin/views/js/scripts.js
+++ core/admin/views/js/scripts.js
@@ -58,6 +58,10 @@ function createFile() {
                     }
                 }
             }
+            let area = item.closest('.img_wrapper');
+            if (area) {
+                dragAndDrop(area, item);
+            }
         })
         let form = document.querySelector('#main-form');
         //
@@ -112,6 +116,24 @@ function createFile() {
                 container.querySelector('img').setAttribute('src', e.target.result);
                 container.classList.remove('empty_container');
             }
+        }
+        function dragAndDrop(area, input) {
+            ['dragenter', 'dragover', 'dragleave', 'drop'].forEach( (eventName, index) => {
+                area.addEventListener(eventName, e => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if (index < 2) {
+                        area.style.background = 'lightblue';
+                    }
+                    else {
+                        area.style.background = '#fff';
+                        if (index === 3) {
+                            input.files = e.dataTransfer.files;
+                            input.dispatchEvent(new Event('change'));
+                        }
+                    }
+                })
+            } )
         }
     }
 }
