e47aaa0528636bf00ee0c91dd697a4d0621eb316
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-02-16 12:35:50 +0100

    Lesson 106

--- core/admin/views/include/footer.php
+++ core/admin/views/include/footer.php
@@ -11,6 +11,7 @@
             <script>
                 const PATH = '<?=PATH?>';
                 const ADMIN_MODE = 1;
+                const tynyMceDefautAreas = '<?=implode(',', $this->blocks['vg-content'])?>'
             </script>
             <?php $this->getScripts(); ?>
     </body>
