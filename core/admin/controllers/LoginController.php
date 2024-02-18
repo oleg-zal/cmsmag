7ac3fd407e54375a8312a1ecae37a814bfb9942d
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-02-18 14:29:20 +0100

    Lesson 116

--- core/admin/controllers/LoginController.php
+++ core/admin/controllers/LoginController.php
@@ -4,12 +4,16 @@ namespace core\admin\controllers;
 
 use core\base\controllers\BaseController;
 use core\base\models\UserModel;
+use core\base\settings\Settings;
 
 class LoginController extends BaseController
 {
     protected $model;
     protected function inputData() {
         $this->model = UserModel::instance();
-        $a=1;
+        if ($this->isPost()) {
+            $a=1;
+        }
+        return $this->render('', ['adminPath' => Settings::get('routes')['admin']['alias']]);
     }
 }
