5888ae34d3040157f4a65b984a49b4098b892750
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-03-18 17:11:47 +0100

    Lesson 152-2

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     header('content-type: text/html; charset=utf-8');
     session_start();
     require_once 'config.php';
+    require_once 'secret.php';
     require_once 'core/base/settings/internal_settings.php';
     require_once 'libraries/functions.php';
     use core\base\exceptions\RouteException;
